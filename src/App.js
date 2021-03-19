8b416ce0f6d8795419474173c00ee653d8ab36bb
Ranvir Raheja <ranvirraheja@Ranvirs-MacBook-Air.local>
2021-03-19 09:49:17 -0700

    debugging needed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+    }
+  
+render () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Weather App</h1>
+      <p>A five day forecast</p>
     </div>
   );
-}
+ }  
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,39 @@
+let data = [
+    {
+        date: "Mar 17, 2021",
+        weather: "70\u2109",
+        icon: "cloudy"
+    },
+    {
+        date: "Mar 18, 2021",
+        weather: "80\u2109",
+        icon: "sunny"
+    },
+    {
+        date: "Mar 19, 2021",
+        weather: "60\u2109",
+        icon: "rainy"
+    },
+    {
+        date: "Mar 20, 2021",
+        weather: "73\u2109",
+        icon: "cloudy"
+    },
+    {
+        date: "Mar 21, 2021",
+        weather: "79\u2109",
+        icon: "sunny"
+    },
+    {
+        date: "Mar 22, 2021",
+        weather: "81\u2109",
+        icon: "sunny"
+    },
+    {
+        date: "Mar 23, 2021",
+        weather: "62\u2109",
+        icon: "rainy"
+    },
+]
+
+export default data 
